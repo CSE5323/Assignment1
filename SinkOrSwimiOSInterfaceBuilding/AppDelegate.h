0dec0a7be9d7597145791d6fab19c031364f2a41
PrestonT <preston_tighe500@yahoo.com>
2016-09-01 14:24:22 -0500

    Initial Commit

---
+++ SinkOrSwimiOSInterfaceBuilding/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SinkOrSwimiOSInterfaceBuilding
+//
+//  Created by Preston Tighe on 9/1/16.
+//  Copyright © 2016 Mobile Design Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SinkOrSwimiOSInterfaceBuilding/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SinkOrSwimiOSInterfaceBuilding
+//
+//  Created by Preston Tighe on 9/1/16.
+//  Copyright © 2016 Mobile Design Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
